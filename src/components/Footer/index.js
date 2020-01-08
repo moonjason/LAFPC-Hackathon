288cf51445d3e9e770f0d4e20c77fe8fdbbbf038
Eder Canada <canada.eder@gmail.com>
2020-01-08 11:59:46 -0800

    remove donate button on the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,25 +4,24 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import Button from '@material-ui/core/Button';
+import logo from '../AdminButton/img/FPC-Logo.png'
+
+// <img src={logo}/>
 
 class Footer extends Component{
     render(){
         return(
             <MainContainer>
                 <Container1>
-                    <LeftDiv>
-                        <h3>STAY CONNECTED</h3>
+                    <p>Copyright © 2020 Los Angeles Food Policy Council. All Rights Reserved</p>
+                </Container1>
+                <Container2>
+                    <div>
+                        {/* <h3>STAY CONNECTED</h3> */}
                         <a href="https://www.facebook.com/LosAngelesFoodPolicyCouncil"><FacebookIcon href="https://www.facebook.com/LosAngelesFoodPolicyCouncil" style={{margin:'10px'}}/></a> 
                         <a href="https://www.instagram.com/lagoodfood/"><InstagramIcon style={{margin:'10px'}}/></a>
                         <a href="https://twitter.com/GoodFoodLA"><TwitterIcon style={{margin:'10px'}}/></a>
-                    </LeftDiv>
-                    <RightDiv>
-                        <h3>GET INVOLVED</h3>
-                        <Button variant="contained" style={{backgroundColor:'orange'}}>DONATE</Button>
-                    </RightDiv>
-                </Container1>
-                <Container2>
-                    <p>Copyright © 2018 Los Angeles Food Policy Council. All Rights Reserved</p>
+                    </div>  
                 </Container2>
             </MainContainer>
         )
